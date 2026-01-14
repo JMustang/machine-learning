67f2c2fa6ea6b1cbfb164662f68b0328e01337b5
Junior <eecfredes@gmail.com>
2026-01-13 22:54:54 -0300

    :rocket:Plotando gráfico, Cerveja vs Nota

--- regressao.py
+++ regressao.py
@@ -9,6 +9,28 @@ df.head()
 
 from sklearn import linear_model
 
-x = df[["cerveja"]]
-y = df[["nota"]]
+X = df[["cerveja"]]
+y = df["nota"]
+
+reg = linear_model.LinearRegression()
+reg.fit(X, y)
+# %%
+
+a, b = reg.intercept_, reg.coef_[0]
+print(a, b)
+print(f"Nota = {a} + {b} * cerveja")
+# %%
+
+predict = reg.predict(X.drop_duplicates())
+predict
+# %%
+
+import matplotlib.pyplot as plt
+
+plt.plot(X["cerveja"], y, "o")
+plt.grid(True)
+plt.title("Relação Cerveja vs Nota")
+plt.xlabel("Ceveja")
+plt.ylabel("Nota")
+plt.plot(X.drop_duplicates()["cerveja"], predict, color="red")
 # %%

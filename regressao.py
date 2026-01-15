d373f5c9648ec4eb3b842efc8c63fe4956f38b29
Junior <eecfredes@gmail.com>
2026-01-14 23:32:25 -0300

    :rocket:Arvore de decisão

--- regressao.py
+++ regressao.py
@@ -8,21 +8,23 @@ df.head()
 # %%
 
 from sklearn import linear_model
+from sklearn import tree
 
 X = df[["cerveja"]]
 y = df["nota"]
 
-reg = linear_model.LinearRegression()
+# Aprendizado de maquina
+reg = linear_model.LinearRegression(fit_intercept=True)
 reg.fit(X, y)
-# %%
 
 a, b = reg.intercept_, reg.coef_[0]
-print(a, b)
-print(f"Nota = {a} + {b} * cerveja")
-# %%
 
-predict = reg.predict(X.drop_duplicates())
-predict
+predict_reg = reg.predict(X.drop_duplicates())
+
+arvore_full = tree.DecisionTreeRegressor(random_state=42)
+arvore_full.fit(X, y)
+
+predict_arvore_full = arvore_full.predict(X.drop_duplicates())
 # %%
 
 import matplotlib.pyplot as plt
@@ -32,5 +34,10 @@ plt.grid(True)
 plt.title("Relação Cerveja vs Nota")
 plt.xlabel("Ceveja")
 plt.ylabel("Nota")
-plt.plot(X.drop_duplicates()["cerveja"], predict, color="red")
+
+
+plt.plot(X.drop_duplicates()["cerveja"], predict_reg, color="red")
+plt.plot(X.drop_duplicates()["cerveja"], predict_arvore_full)
+
+plt.legend(["Observação", f"y = {a:.3f} + {b:.3f} X", "Arvore Full"])
 # %%

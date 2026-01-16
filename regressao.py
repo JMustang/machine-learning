b4079fc7b2b8c1880560976953270aff7cd0709e
Junior <eecfredes@gmail.com>
2026-01-15 22:31:39 -0300

    :rocket:plotando uma arvore de decisão

--- regressao.py
+++ regressao.py
@@ -23,8 +23,11 @@ predict_reg = reg.predict(X.drop_duplicates())
 
 arvore_full = tree.DecisionTreeRegressor(random_state=42)
 arvore_full.fit(X, y)
-
 predict_arvore_full = arvore_full.predict(X.drop_duplicates())
+
+arvore_d2 = tree.DecisionTreeRegressor(random_state=42, max_depth=2)
+arvore_d2.fit(X, y)
+predict_arvore_d2 = arvore_d2.predict(X.drop_duplicates())
 # %%
 
 import matplotlib.pyplot as plt
@@ -37,7 +40,17 @@ plt.ylabel("Nota")
 
 
 plt.plot(X.drop_duplicates()["cerveja"], predict_reg, color="red")
-plt.plot(X.drop_duplicates()["cerveja"], predict_arvore_full)
+plt.plot(X.drop_duplicates()["cerveja"], predict_arvore_full, color="blue")
+plt.plot(
+    X.drop_duplicates()["cerveja"], predict_arvore_d2, color="green", linestyle="--"
+)
 
-plt.legend(["Observação", f"y = {a:.3f} + {b:.3f} X", "Arvore Full"])
+plt.legend(
+    ["Observação", f"y = {a:.3f} + {b:.3f} X", "Arvore Full", "Arvore Depth = 2"]
+)
+# %%
+
+plt.figure(dpi=400)
+
+tree.plot_tree(arvore_d2, feature_names=["cerveja"], filled=True)
 # %%
